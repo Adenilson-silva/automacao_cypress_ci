4f9cbb6c07e3fc5862e8fdf41574ecd07f7e4f6f
Adenilson Silva <adenilson.as@gmail.com>
2025-10-28 20:33:59 -0300

    Criação e melhoria de casos de teste

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,22 +5,59 @@ describe('Conjunto de Teste', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+  afterEach(() => {
+		cy.clearAllCookies();
+		cy.clearAllLocalStorage();
+    cy.clearAllSessionStorage();
+	})
 
-  context('Cadastros de Usuários', () => {
-    it('Registrar Usuário válido', () => {
-      let primeiroNome = chance.first();
-      let ultimoNome = chance.last();
-      let nome = primeiroNome + " " + ultimoNome
-      let email = chance.email();
-      let dia = chance.integer({ min: 1, max: 28 });
-      let mes = chance.integer({ min: 1, max: 12 });
-      let ano = chance.year({min: 1900, max: 2000});
-      let empresa = chance.company();
-      let endereco = chance.address();
-      let state = chance.state();
-      let city = chance.city();
-      let zipcode = chance.zip();
-      let phone = chance.phone();
+  context('Cadastro de Novos Usuários', () => {
+    it('Verificar campos não preenchidos e com valores inválidos', () => {
+      const primeiroNome = chance.first();
+      const ultimoNome = chance.last();
+      const nome = primeiroNome + " " + ultimoNome
+      cy.get('#header a[href="/login"]').click();
+      cy.get('#form div.signup-form h2').should('have.text', 'New User Signup!');
+      cy.get('[data-qa="signup-button"]').click();
+      cy.get('[data-qa="signup-name').then(($input) => {
+        expect($input[0].checkValidity()).to.be.false
+        expect($input[0].validationMessage).to.eq('Preencha este campo.')
+      });
+      cy.get('[data-qa="signup-name"]').type(nome);
+      cy.get('[data-qa="signup-button"]').click();
+      cy.get('[data-qa="signup-email"]').then(($input) => {
+        expect($input[0].checkValidity()).to.be.false
+        expect($input[0].validationMessage).to.eq('Preencha este campo.')
+      });
+      cy.get('[data-qa="signup-email"]').type(primeiroNome);
+      cy.get('[data-qa="signup-button"]').click();
+      cy.get('[data-qa="signup-email"]').then(($input) => {
+        expect($input[0].checkValidity()).to.be.false
+        expect($input[0].validationMessage).to.include('Inclua um "@" no endereço de e-mail')
+      });
+      cy.get('[data-qa="signup-email"]').clear().type(primeiroNome + "@");
+      cy.get('[data-qa="signup-button"]').click();
+      cy.get('[data-qa="signup-email"]').then(($input) => {
+        expect($input[0].checkValidity()).to.be.false
+        expect($input[0].validationMessage).to.include('Insira uma parte depois de')
+      });
+      cy.location('pathname').should('equal', '/login');
+    })
+
+    it('Registrar um Novo Usuário Válido', () => {
+      const primeiroNome = chance.first();
+      const ultimoNome = chance.last();
+      const nome = primeiroNome + " " + ultimoNome
+      const email = chance.email();
+      const dia = chance.integer({ min: 1, max: 28 });
+      const mes = chance.integer({ min: 1, max: 12 });
+      const ano = chance.year({ min: 1900, max: 2000 });
+      const empresa = chance.company();
+      const endereco = chance.address();
+      const state = chance.state();
+      const city = chance.city();
+      const zipcode = chance.zip();
+      const phone = chance.phone();
       cy.get('#slider-carousel div.active h1').contains('AutomationExercise');
       cy.get('#header a[href="/login"]').click();
       cy.get('#form div.signup-form h2').should('have.text', 'New User Signup!');
@@ -32,7 +69,7 @@ describe('Conjunto de Teste', () => {
       cy.get('#id_gender1').check();
       cy.get('[data-qa="name"]').should('have.value', nome);
       cy.get('[data-qa="email"]').should('have.value', email);
-      cy.get('[data-qa="password"]').type('123456789', {log: false});
+      cy.get('[data-qa="password"]').type('123456789', { log: false });
       cy.get('[data-qa="days"]').select(dia);
       cy.get('[data-qa="months"]').select(mes);
       cy.get('[data-qa="years"]').select(ano);
@@ -54,11 +91,47 @@ describe('Conjunto de Teste', () => {
     })
   })
 
-  context('Teste com usuário logado', () => {
-    it('Realizar Login', () => {
+  context('Login', () => {
+    it('Verificar campos não preenchidos e com valores inválidos', () => {
+      cy.get('#header a[href="/login"]').click();
+      cy.get('[data-qa="login-button"]').click();
+      cy.get('[data-qa="login-email"]').then(($input) => {
+        expect($input[0].checkValidity()).to.be.false
+        expect($input[0].validationMessage).to.eq('Preencha este campo.')
+      });
+
+      cy.get('[data-qa="login-email"]').type("teste");
+      cy.get('[data-qa="login-button"]').click();
+      cy.get('[data-qa="login-email"]').then(($input) => {
+        expect($input[0].checkValidity()).to.be.false
+        expect($input[0].validationMessage).to.include('Inclua um "@" no endereço de e-mail')
+      });
+      cy.get('[data-qa="login-email"]').clear().type("teste@");
+      cy.get('[data-qa="login-button"]').click();
+      cy.get('[data-qa="login-email"]').then(($input) => {
+        expect($input[0].checkValidity()).to.be.false
+        expect($input[0].validationMessage).to.include('Insira uma parte depois de')
+      });
+
+      cy.fixture('usuarios').then((data) => {
+        const usuario = data.usuarios[0]
+        cy.get('[data-qa="login-email"]').clear().type(usuario.email);
+        cy.get('[data-qa="login-button"]').click();
+        cy.get('[data-qa="login-password"]').then(($input) => {
+          expect($input[0].checkValidity()).to.be.false
+          expect($input[0].validationMessage).to.eq('Preencha este campo.')
+        })
+        cy.get('[data-qa="login-password"]').type(chance.integer());
+        cy.get('[data-qa="login-button"]').click();
+      })
+      //cy.get('#form p').should('have.text', 'Your email or password is incorrect!');
+      //cy.location('pathname').should('equal', '/login');
+    })
+
+    it('Realizar Login (usuário válido)', () => {
       cy.get('#header a[href="/login"]').click();
       cy.fixture('usuarios').then((data) => {
-        let usuario = data.usuarios[0]
+        const usuario = data.usuarios[0]
         cy.get('[data-qa="login-email"]').type(usuario.email);
         cy.get('[data-qa="login-password"]').type(usuario.senha, { log: false });
         cy.get('[data-qa="login-button"]').click();
@@ -67,13 +140,39 @@ describe('Conjunto de Teste', () => {
       })
     })
 
+    it('Logout', () => {
+      cy.get('#header a[href="/login"]').click();
+      cy.fixture('usuarios').then((data) => {
+        const usuario = data.usuarios[0]
+        cy.login(usuario.email, usuario.senha)
+      })
+      cy.visit('/');
+      cy.get('#header a[href="/logout"]').click();
+      cy.get('#form div.login-form h2').should('have.text', 'Login to your account');
+      cy.location('pathname').should('equal', '/login');
+    })
+  })
+
+  context('Compras', () => {
     it('Comprar', () => {
       cy.get('#header a[href="/login"]').click();
       cy.fixture('usuarios').then((data) => {
-        let usuario = data.usuarios[0]
+        const usuario = data.usuarios[0]
         cy.login(usuario.email, usuario.senha)
       })
-      cy.visit('/')
+      cy.visit('/', {
+        onBeforeLoad: (contentWindow) => {
+          contentWindow.performance.mark('inicio');
+        },
+        onLoad: (contentWindow) => {
+          contentWindow.performance.mark('fim');
+          contentWindow.performance.measure('carregamentoDaPagina', 'inicio', 'fim');
+        }
+      }).then((win) => {
+        const tempoDeCarregamento = win.performance.getEntriesByName('carregamentoDaPagina')[0].duration;
+        cy.log(`Tempo de Carregamento: ${tempoDeCarregamento}`);
+        console.log(`Tempo de Carregamento: ${tempoDeCarregamento}`);
+      });
       cy.get('a[href="/product_details/1"]').click();
       cy.get('button.cart').click();
       cy.get('#cartModal h4.modal-title').should('have.text', 'Added!');
